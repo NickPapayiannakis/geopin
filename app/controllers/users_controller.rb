e85618ad16f4f51523c10bfa7dec6c598049681e
NickPapayiannakis <nikos.papayiannakis@gmail.com>
2017-02-18 17:14:49 -0500

    Fixed merge conflicts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def delete
+  end
+
+  def login
+  end
+
+  def logout
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/delete'
+
+  get 'users/login'
+
+  get 'users/logout'
+
+  get 'users/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
