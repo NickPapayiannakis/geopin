98e5c6c950845a752bce817749667e652861e3f4
Bruno Passos <bruno.passos@shopify.com>
2017-02-24 16:22:42 -0500

    Home scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,19 @@ Rails.application.routes.draw do
   get 'users/update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'users/new'
+
+  get 'users/delete'
+
+  get 'users/login'
+
+  get 'users/logout'
+
+  get 'users/update'
+
+  get 'login/index'
+  
+  root 'login#index'
+
 end
