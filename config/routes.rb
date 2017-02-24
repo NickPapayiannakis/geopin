de9f67b854173df2f93a9f47a84eabec551d58da
Bruno Passos <bruno.passos@shopify.com>
2017-02-24 16:31:12 -0500

    merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/delete'
-
-  get 'users/login'
-
-  get 'users/logout'
-
-  get 'users/update'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'users/new'
