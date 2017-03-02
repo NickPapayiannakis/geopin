0820ee0784ca4ec12c0393fc9face20f4d4b1d4b
NickPapayiannakis <nikos.papayiannakis@gmail.com>
2017-03-02 08:31:33 -0500

    Fixed issues, added preliminary signup valildation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,21 @@ class UsersController < ApplicationController
   def update
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #handle successful save
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :height, :weight)
+    end
 end
